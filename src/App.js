72ac3d9ae45ae423b807b50a7dafb383f5b1a204
Dev-Dori <trouna43@gmail.com>
2022-03-01 04:28:19 +0900

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App(){
   console.log('App component load')
   var AppList = [
     {
-      name:'Terminal',page:'This page is Terminal - 현재 개발 진행중입니다!!!',
+      name:'Terminal',page:'This page is Terminal - 현재 개발 진행중입니다!!!!',
       state:false, Isdir:false, minsize:{width:400,height:400}, dock:true,
       icon:"./img/icon/Terminal.png"
     },
